8635ba2b6349dd537106ab3048816e9d0e409c44
Naloyka <elena.naloeva@yandex.ru>
2022-06-05 21:02:27 +0300

    loadTaskState

--- js/loadStateTask.js
+++ js/loadStateTask.js
@@ -3,15 +3,21 @@ import { getTaskHTML } from "./getTaskHTML";
 const pasteTask = document.querySelector(".paste__task")
 
 export function loadTaskState() {
-
     const openSaveTasks = JSON.parse(localStorage.getItem("tasks"))
-    console.log(openSaveTasks)
 
-    for (let i = 0; i < openSaveTasks.length; i++) {
-        console.log
-        const textContent = openSaveTasks[i].textContent
-        const checked = Boolean(openSaveTasks[i].checked)
+    if (openSaveTasks === null) {
 
-        pasteTask.insertAdjacentHTML('beforeend', getTaskHTML(textContent, checked, false))
+        return
+
+    } else {
+        for (let i = 0; i < openSaveTasks.length; i++) {
+            console.log
+            const textContent = openSaveTasks[i].textContent
+            const checked = Boolean(openSaveTasks[i].checked)
+
+            pasteTask.insertAdjacentHTML('beforeend', getTaskHTML(textContent, checked, false))
+        }
     }
+
+
 }
--- js/main.js
+++ js/main.js
@@ -118,6 +118,5 @@ function addTask() {
 addTask()
 loadTaskState()
 deleteTask()
-loadTaskState();
 loadRemoteTasks();
 
